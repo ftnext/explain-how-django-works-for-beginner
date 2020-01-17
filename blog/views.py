3b0133546c8a8a565de5a0427f164b666b5378e0
ftnext <takuyafjp+develop@gmail.com>
2020-01-17 18:20:21 +0900

    [feat] 管理画面から作った投稿をテンプレートで表示するように変更（URL、ビュー、モデル、テンプレートが連携して動作）

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def post_list(request):
-    # 指定したテンプレート（HTML）をメッセージ本文に入れた
-    # HTTPレスポンス（HttpResponse）を、render関数で作って返す
-    return render(request, 'blog/post_list.html', {})
+    # ブログ投稿のうち、published_date（公開日）が現在より前のものを取得し。
+    # 公開日の昇順（以前に公開されたものほど上）に並べ替える
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # 取得した投稿データpostsをテンプレートでpostsという名前で扱えるように渡す
+    return render(request, 'blog/post_list.html', {'posts': posts})
